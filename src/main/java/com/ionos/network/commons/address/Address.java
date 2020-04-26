6d0e256b3efb958409bcd36ffd02184eb190f3ff
Stephan Fuhrmann <s_fuhrm@gmx.de>
2020-04-26 10:51:27 +0200

    Optimization for Address.getBytes() for AbstractAddress instances

--- src/main/java/com/ionos/network/commons/address/Address.java
+++ src/main/java/com/ionos/network/commons/address/Address.java
@@ -27,8 +27,22 @@ public interface Address {
      */
     Comparator<Address> COMPONENT_COMPARATOR =
             (o1, o2) -> {
-                byte[] o1Address = o1.getBytes();
-                byte[] o2Address = o2.getBytes();
+                byte[] o1Address;
+                byte[] o2Address;
+
+                // don't call getBytes() if not necessary.
+                // getBytes() creates a copy of the array.
+                if (o1 instanceof AbstractAddress) {
+                    o1Address = ((AbstractAddress)o1).address;
+                } else {
+                    o1Address = o1.getBytes();
+                }
+
+                if (o2 instanceof AbstractAddress) {
+                    o2Address = ((AbstractAddress)o2).address;
+                } else {
+                    o2Address = o2.getBytes();
+                }
 
                 for (int i = 0; i < o1Address.length; i++) {
                     int v1 = o1Address[i] & BYTE_MASK;
--- src/test/java/com/ionos/network/commons/address/AddressTest.java
+++ src/test/java/com/ionos/network/commons/address/AddressTest.java
@@ -8,12 +8,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddressTest {
     @Test
-    public void testComponentComparator() {
+    public void testComponentComparatorWithIP() {
         IP ip1 = new IP("192.168.0.1");
         IP ip2 = new IP("192.168.0.2");
 
         assertEquals(0, Address.COMPONENT_COMPARATOR.compare(ip1, ip1));
         assertEquals(-1, Address.COMPONENT_COMPARATOR.compare(ip1, ip2));
         assertEquals(1, Address.COMPONENT_COMPARATOR.compare(ip2, ip1));
+    }
+
+    @Test
+    public void testComponentComparatorWithOther() {
+        TestAddress first = new TestAddress();
+
+        assertEquals(0, Address.COMPONENT_COMPARATOR.compare(first, first));
+    }
+
+    private class TestAddress implements Address {
+
+        @Override
+        public byte[] getBytes() {
+            return new byte[4];
+        }
     }
 }
