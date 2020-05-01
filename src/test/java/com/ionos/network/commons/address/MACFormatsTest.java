713e7ecdb752473579f4e5fd90fdd3b13c29871a
Stephan Fuhrmann <s@sfuhrm.de>
2020-05-01 13:09:02 +0200

    Simplify

--- src/test/java/com/ionos/network/commons/address/MACFormatsTest.java
+++ src/test/java/com/ionos/network/commons/address/MACFormatsTest.java
@@ -1,10 +1,9 @@
 package com.ionos.network.commons.address;
 
-import com.ionos.network.commons.address.AddressFormat;
-import com.ionos.network.commons.address.MAC;
-import com.ionos.network.commons.address.MACFormats;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static com.ionos.network.commons.address.MACFormats.*;
 
 /** JUnit test for {@link MACFormats}.
  * @author Stephan Fuhrmann
@@ -23,29 +22,37 @@ public class MACFormatsTest {
 
     @Test
     public void testCOLON_SPARATED_HEX_FORMAT() {
-    	final AddressFormat<MAC> formatter = MACFormats.COLON_SEPARATED_HEX_FORMAT;
-        assertEquals("6c:88:14:6f:d8:91", formatter.format(MAC1));
+        assertEquals("6c:88:14:6f:d8:91",
+                COLON_SEPARATED_HEX_FORMAT.format(MAC1));
 
-        assertEquals("00:00:14:6f:d8:91", formatter.format(MAC2));
+        assertEquals("00:00:14:6f:d8:91",
+                COLON_SEPARATED_HEX_FORMAT.format(MAC2));
 
-        assertEquals("14:6f:00:00:d8:91", formatter.format(MAC3));
+        assertEquals("14:6f:00:00:d8:91",
+                COLON_SEPARATED_HEX_FORMAT.format(MAC3));
 
-        assertEquals("14:6f:d8:91:00:00", formatter.format(MAC4));
+        assertEquals("14:6f:d8:91:00:00",
+                COLON_SEPARATED_HEX_FORMAT.format(MAC4));
 
-        assertEquals("00:00:00:00:00:00", formatter.format(MAC5));
+        assertEquals("00:00:00:00:00:00",
+                COLON_SEPARATED_HEX_FORMAT.format(MAC5));
     }
 
     @Test
     public void testCISCO_CUSTOM_FORMAT() {
-    	final AddressFormat<MAC> formatter = MACFormats.CISCO_CUSTOM_FORMAT;
-        assertEquals("6c88.146f.d891", formatter.format(MAC1));
+        assertEquals("6c88.146f.d891",
+                CISCO_CUSTOM_FORMAT.format(MAC1));
 
-        assertEquals("0000.146f.d891", formatter.format(MAC2));
+        assertEquals("0000.146f.d891",
+                CISCO_CUSTOM_FORMAT.format(MAC2));
 
-        assertEquals("146f.0000.d891", formatter.format(MAC3));
+        assertEquals("146f.0000.d891",
+                CISCO_CUSTOM_FORMAT.format(MAC3));
 
-        assertEquals("146f.d891.0000", formatter.format(MAC4));
+        assertEquals("146f.d891.0000",
+                CISCO_CUSTOM_FORMAT.format(MAC4));
 
-        assertEquals("0000.0000.0000", formatter.format(MAC5));
+        assertEquals("0000.0000.0000",
+                CISCO_CUSTOM_FORMAT.format(MAC5));
     }
 }
